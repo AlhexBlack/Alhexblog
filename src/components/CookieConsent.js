3fe405e8351034d36d0c69b5f9a1d93f1473ff49
AlhexBlack <michealunuafeh@gmail.com>
2024-09-10 01:13:00 +0100

    optimized web speed

--- src/components/CookieConsent.js
+++ src/components/CookieConsent.js
@@ -17,18 +17,29 @@ function CookieConsent() {
         setShowPopup(false);
     };
 
+    const handleRejectCookies = () => {
+        localStorage.setItem('cookieConsent', 'false');
+        setShowPopup(true);
+    };
+    
+
     return (
         showPopup && (
             <div className="cookie-consent">
                 <p>
                     This website uses cookies to ensure you get the best experience on our website.{' '}
-                    <Link to ="/privacy-policy" target="_blank" rel="noopener noreferrer">
+                    <Link to ="/privacy-policy"  rel="noopener noreferrer">
                         Learn more
                     </Link>
                 </p>
+                <div className='cookieBtnDiv'>
+                <button onClick={handleRejectCookies} className="reject-btn">
+                    Reject
+                </button>
                 <button onClick={handleAcceptCookies} className="accept-btn">
                     Accept Cookies
                 </button>
+                </div>
             </div>
         )
     );
